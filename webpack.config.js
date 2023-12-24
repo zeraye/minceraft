ec21e23ffddf1b7d497e68fb5ee15d1b1549537e
zeraye <zeraye@protonmail.com>
2023-12-24 23:37:56 +0100

    feat: use raw-loader for .glsl files

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,10 @@ module.exports = {
         use: "ts-loader",
         exclude: /node_modules/,
       },
+      {
+        test: /\.glsl$/i,
+        use: "raw-loader",
+      },
     ],
   },
   resolve: {

0c732d322335679ba1c64883f0af695fba5a04fa
zeraye <zeraye@protonmail.com>
2023-12-26 17:17:36 +0100

    feat: add fragment and vertex shaders

---
+++ src/shaders/vertex.glsl
@@ -0,0 +1,15 @@
+#version 300 es
+precision mediump float;
+
+uniform mat4 matViewProj;
+uniform mat4 matWorld;
+
+in vec3 vertexPosition;
+in vec3 vertexColor;
+
+out vec3 fragmentColor;
+
+void main() {
+  fragmentColor = vertexColor;
+  gl_Position = matViewProj * matWorld * vec4(vertexPosition, 1.0);
+}

b088c33b69e887b6ed447aca6f5418a3f765a713
zeraye <zeraye@protonmail.com>
2023-12-26 18:57:04 +0100

    feat: add texture in shaders and comment out color

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,10 +1,14 @@
 #version 300 es
 precision mediump float;
 
-in vec3 fragmentColor;
+uniform sampler2D uSampler;
+
+// in vec3 fragmentColor;
+in vec2 fragmentTexture;
 
 out vec4 outputColor;
 
 void main() {
-  outputColor = vec4(fragmentColor, 1.0);
+  // outputColor = vec4(fragmentColor, 1.0);
+  outputColor = texture(uSampler, fragmentTexture);
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -5,11 +5,14 @@ uniform mat4 matViewProj;
 uniform mat4 matWorld;
 
 in vec3 vertexPosition;
-in vec3 vertexColor;
+// in vec3 vertexColor;
+in vec2 vertexTexture;
 
-out vec3 fragmentColor;
+// out vec3 fragmentColor;
+out vec2 fragmentTexture;
 
 void main() {
-  fragmentColor = vertexColor;
+  // fragmentColor = vertexColor;
   gl_Position = matViewProj * matWorld * vec4(vertexPosition, 1.0);
+  fragmentTexture = vertexTexture;
 }
